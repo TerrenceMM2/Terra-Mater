d37a8d032abfa3808f51f72fc2aaa45c221ee28a
pete <petewanca@gmail.com>
2019-08-07 23:22:07 -0500

    schema and seed added, old model removed

--- models/plant-model.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-    var Plant = sequelize.define('plants', {
-      text: DataTypes.STRING,
-      description: DataTypes.TEXT
-    });
-    return Plant;
-  };
-  
