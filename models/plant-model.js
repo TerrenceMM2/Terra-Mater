fec87c42032e3832a87ae8420fd276c1f2d4f8e9
pete <petewanca@gmail.com>
2019-08-07 23:46:07 -0500

    re-added plant models for querying

---
+++ models/plant-model.js
@@ -0,0 +1,56 @@
+module.exports = function(sequelize, DataTypes) {
+    var Plant = sequelize.define('Plants', {
+        commonName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        scienName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        shortDesc: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        longDesc: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        poisHuman: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        poisAnimal: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        poisNotes: {
+            type: DataTypes.TEXT,
+        },
+        genCare: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        food: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        water: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        minTemp: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        img: {
+            type: DataTypes.BLOB,
+            allowNull: false
+        },
+        sunlight: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+    });
+    return Plant;
+};  
