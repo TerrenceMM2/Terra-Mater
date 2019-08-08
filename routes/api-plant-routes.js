ad44edfd33c0794a4fd7eb24a63a4ded6f9cf099
pete <petewanca@gmail.com>
2019-08-08 00:33:39 -0500

    attempting get route for all plant data

--- routes/api-plant-routes.js
+++ routes/api-plant-routes.js
@@ -1,11 +1,18 @@
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
 
+    // @READ route gets all plant data
     app.get("/api/plant", function(req, res) {
-        
 
-        res.json("The plant API works!");
+        db.Plant.findAll({}).then(function(results) {
+            res.json(results);
+        });
+
     });
 
+
 };  
+
+// for pulling images out of BLOB datatype...
+// https://stackoverflow.com/questions/9042327/node-js-reading-blob-from-mysql
