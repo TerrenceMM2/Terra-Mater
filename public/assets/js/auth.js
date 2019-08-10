560f71fb58632da7c07d76401e2440409e7b0ba2
Josh Latour <jlatour88@gmail.com>
2019-08-10 13:57:07 -0500

    grab error json and display error msg to user

--- public/assets/js/auth.js
+++ public/assets/js/auth.js
@@ -38,9 +38,20 @@ $('.error-msg').text('Loading...')
             }
          }).catch(function(err){
             console.log(err)
+            if (err.responseJSON.type == "formError") {
+               console.log('Form Error');
+               $('.error-msg').text(err.responseJSON.errors[0].msg);
+            }
 
-
-            $('.error-msg').text(err.responseJSON.msg)
+            if (err.responseJSON.type == "dbError") {
+               console.log('Database Error');
+               if (err.responseJSON.error.original.code === 'ER_DUP_ENTRY') {
+                  // duplicate email found
+                  $('.error-msg').text('Email already exists. Please Login instead');
+               } else {
+                  $('.error-msg').text(err.responseJSON.msg);
+               }
+            }
          })
    }); // End of Registration
 
