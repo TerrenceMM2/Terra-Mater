0b2011a1865e7393595bbf7176fd552bc35bff00
Micaela Bray <micaela.bray@gmail.com>
2019-08-10 08:34:30 -0500

    creates branch for api-routes-micaela and starts working on routes

--- routes/api-plant-routes.js
+++ routes/api-plant-routes.js
@@ -6,4 +6,20 @@ module.exports = function (app) {
         res.json("The plant API works!");
     });
 
+    app.get("api/plant/care/:id", function(req,res){
+        if (req.params.db) {
+            db.findOne({
+              where: {
+                routeName: req.params.text
+              }
+            }).then(function(result) {
+              return res.json(result);
+            });
+          } else {
+            db.findAll().then(function(result) {
+              return res.json(result);
+            });
+          }
+    })
+
 };  
