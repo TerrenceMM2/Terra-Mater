772d419c5c19026de7ec5036109423d8a25468da
Josh Latour <jlatour88@gmail.com>
2019-08-11 00:00:18 -0500

    eslint formating

--- models/user-model.js
+++ models/user-model.js
@@ -1,30 +1,29 @@
 module.exports = function(sequelize, DataTypes) {
   var Users = sequelize.define("Users", {
-      email: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          unique: true,
-          validate: {
-              isEmail: true,
-              notEmpty: true
-          }
-      },
-      password: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-              len: [8, 255]
-          }
-      },
-      firstName: {
-          type: DataTypes.STRING,
-          allowNull: false
-      },
-      lastName: {
-          type: DataTypes.STRING,
-          allowNull: false
-      },
-
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+        notEmpty: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, 255]
+      }
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   });
 
   return Users;
