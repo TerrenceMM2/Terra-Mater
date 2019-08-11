4fa506f20d7a0fca0d7897339c5635f4a9193d58
Josh Latour <jlatour88@gmail.com>
2019-08-11 00:02:12 -0500

    eslint formatting

--- public/assets/js/auth.js
+++ public/assets/js/auth.js
@@ -1,63 +1,62 @@
 $(document).ready(function() {
+  // Send Registration data to Database
+  $("#register").on("submit", function(event) {
+    $(".error-msg").text("Loading...");
 
-   // Send Registration data to Database
-   $('#register').on('submit', function(event) {
-$('.error-msg').text('Loading...')
+    event.preventDefault();
 
-      event.preventDefault();
-      
-         // Write Data to Object
-         var data = {
-            firstName: $('#firstName')
-               .val()
-               .trim(),
-            lastName: $('#lastName')
-               .val()
-               .trim(),
-            email: $('#email')
-               .val()
-               .trim(),
-            password: $('#userPassword')
-               .val()
-               .trim(),
-            password2: $('#password2')
-               .val()
-               .trim()
-         };
-         console.log(data);
-   
-         // Pass Data object to DB
-         $.ajax({
-            method: 'post',
-            url: '/register',
-            data: data
-         })
-         .then(function(res) {
-            if (res.success){
-               window.location.href= "/user-profile"
-            }
-         }).catch(function(err){
-            console.log(err)
+    // Write Data to Object
+    var data = {
+      firstName: $("#firstName")
+        .val()
+        .trim(),
+      lastName: $("#lastName")
+        .val()
+        .trim(),
+      email: $("#email")
+        .val()
+        .trim(),
+      password: $("#userPassword")
+        .val()
+        .trim(),
+      password2: $("#password2")
+        .val()
+        .trim()
+    };
+    console.log(data);
 
-            // Check if Form error
-            if (err.responseJSON.type == "formError") {
-               console.log('Form Error');
-               $('.error-msg').text(err.responseJSON.formErrors[0].msg);
-            }
+    // Pass Data object to DB
+    $.ajax({
+      method: "post",
+      url: "/register",
+      data: data
+    })
+      .then(function(res) {
+        if (res.success) {
+          window.location.href = "/user-profile";
+        }
+      })
+      .catch(function(err) {
+        console.log(err);
 
-            // Check if DB error
-            if (err.responseJSON.type == "dbError") {
-               console.log('Database Error');
+        // Check if Form error
+        if (err.responseJSON.type === "formError") {
+          console.log("Form Error");
+          $(".error-msg").text(err.responseJSON.formErrors[0].msg);
+        }
 
-               // Check for duplicate email found
-               if (err.responseJSON.error.original.code === 'ER_DUP_ENTRY') {
-                  $('.error-msg').text('Email already exists. Please Login instead');
-               } else {
-                  // Generic error
-                  $('.error-msg').text(err.responseJSON.msg);
-               }
-            }
-         })
-   }); // End of Registration function
+        // Check if DB error
+        if (err.responseJSON.type === "dbError") {
+          console.log("Database Error");
 
+          // Check for duplicate email found
+          if (err.responseJSON.error.original.code === "ER_DUP_ENTRY") {
+            $(".error-msg").text("Email already exists. Please Login instead");
+          } else {
+            // Generic error
+            $(".error-msg").text(err.responseJSON.msg);
+          }
+        }
+      });
+  }); // End of Registration function
 });
