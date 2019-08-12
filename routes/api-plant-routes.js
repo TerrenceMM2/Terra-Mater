894f9d46205800e2c2e0c415b0d24ae3500db514
Micaela Bray <micaela.bray@gmail.com>
2019-08-11 21:26:15 -0500

    updates plants route to clean up merged code

--- routes/api-plant-routes.js
+++ routes/api-plant-routes.js
@@ -3,54 +3,50 @@ var Sequelize = require("sequelize");
 var Op = Sequelize.Op;
 
 module.exports = function(app) {
-  app.get("/api/plants", function(req, res) {
-    db.Plants.findAll()
-      .then(function(result) {
-        res.json(result);
-      })
-      .catch(function(err) {
-        res.json(err);
-      });
-  });
+    app.get("/api/plants", function(req, res) {
+        db.Plants.findAll()
+        .then(function(result) {
+            res.json(result);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+    });
 
-<<<<<<< HEAD
     app.get("/api/plant", function(req, res) {
-        
-
         res.json("The plant API works!");
     });
 
     //Update plant care
-    app.put("/api/plant/:id", function(req, res) {
-        db.Plant.update({
-            text: req.body.text,
-            complete: req.body.complete
-          }, {
-            where: {
-              id: req.body.id
+    // app.put("/api/plant/:id", function(req, res) {
+    //     db.Plant.update({
+    //         text: req.body.text,
+    //         complete: req.body.complete
+    //       }, {
+    //         where: {
+    //           id: req.body.id
+    //         }
+    //       }).then(function(dbPlant) {
+    //         res.json(dbPlant);
+    //       });
+    //     });
+    
+    app.get("/api/plants/search", function(req, res) {
+        db.Plants.findAll({
+        where: {
+            commonName: {
+            // $like - does not work
+            [Op.substring]: req.body.plantSearch
             }
-          }).then(function(dbPlant) {
-            res.json(dbPlant);
-          });
-        });
-
-};  
-=======
-  app.get("/api/plants/search", function(req, res) {
-    db.Plants.findAll({
-      where: {
-        commonName: {
-          // $like - does not work
-          [Op.substring]: req.body.plantSearch
         }
-      }
-    })
-      .then(function(result) {
-        res.json(result);
-      })
-      .catch(function(err) {
-        res.json(err);
-      });
-  });
-};
->>>>>>> 3c1ea48d6d407c0ddf1c948dd8ee28eae9b81d4d
+        })
+        .then(function(result) {
+            res.json(result);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+    });
+};  
+  
+
