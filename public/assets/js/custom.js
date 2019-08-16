e6b6ab44f482bc19b7c516ebdf1a1978bbecdbc1
TerrenceMM2 <terrencemm2@gmail.com>
2019-08-16 13:19:59 -0500

    adds green mode and restyles search (home) page

---
+++ public/assets/js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("#greenModeToggle").on("click", function() {
+    if ($("#greenModeToggle").prop("checked")) {
+      $("#green-mode-stylesheet").attr("href", "/assets/css/style_green.css");
+      $(".terra-mater-logo-wide").attr("src", "/assets/images/tm-logo-wide-invert.png");
+      $(".terra-mater-logo").attr("src", "/assets/images/tm-logo-invert.png");
+    } else {
+      $("#green-mode-stylesheet").attr("href", "/assets/css/style_normal.css");
+      $(".terra-mater-logo-wide").attr("src", "/assets/images/tm-logo-wide.png");
+      $(".terra-mater-logo").attr("src", "/assets/images/tm-logo.png");
+    }
+  });
+});
