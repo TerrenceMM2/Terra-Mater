b97f84c9d27db880ac8206f941ff80d7ac5fd434
TerrenceMM2 <terrencemm2@gmail.com>
2019-08-17 11:31:43 -0500

    adds screenshots to be used in README.

--- db/schema.sql
+++
@@ -1,6 +0,0 @@
---Use if you wish to create your own database for replicating our product on your machine
---The seed file contains table layout and csv contains sample dataset
-
-DROP DATABASE IF EXISTS terraMater;
-
-CREATE DATABASE terraMater;
